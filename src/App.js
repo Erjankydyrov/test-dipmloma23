228e6b7e747f7a9bce5dbe7853518ca0406bbcc9
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-03 14:59:11 +0600

    Fix the issue that Netlify was complaining about

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
       <h1>Hello world</h1>
 
       <h2>Bye</h2>
-      <img src="https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/hero_small/99i33zyc0l_Large_WW170579.jpg" />
+      <img src="https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/hero_small/99i33zyc0l_Large_WW170579.jpg" alt="Panda" />
     </div>
   );
 }
