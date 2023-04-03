eef930c84319fedc0763aa9138246b311d6c11d1
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-03 14:56:00 +0600

    Added a code that should trigger an error.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      <h2>Bye</h2>
+      <img src="https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/hero_small/99i33zyc0l_Large_WW170579.jpg" />
     </div>
   );
 }
