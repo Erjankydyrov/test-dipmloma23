0a03bd54ff454f773e8d2ad2024e1773536da788
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-03 15:50:49 +0600

    Added React Router to the App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ function App() {
     <div className="App">
       <h1>Hello world</h1>
 
-      <h2>Bye</h2>
-      <img src="https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/hero_small/99i33zyc0l_Large_WW170579.jpg" alt="Panda" />
+      
     </div>
   );
 }
