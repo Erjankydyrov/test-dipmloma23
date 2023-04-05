3f73e2759357187594b311eeee6d678758fd3a24
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-05 15:49:54 +0600

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="/contacts" element={<h1>Contacts</h1>} />
-          <Route path="/delivery" element={<h1>Deliver</h1>} />
+          <Route path="/delivery" element={<h1>Delivery</h1>} />
         </Routes>
       </Layout>
     </div>
