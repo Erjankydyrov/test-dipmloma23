cc08a8a6b8ee621b9aa09a320698e6cb19d7899b
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-07 15:50:46 +0600

    Add NotFound page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,4 +1,5 @@
 import { useMatch } from "react-router-dom";
+import NotFound from "./NotFound";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
@@ -13,6 +14,10 @@ export default function Category() {
   const category = categories.find(
     (category) => category.slug === params.slug
   );
+
+  if (!category) {
+    return <NotFound />
+  }
 
   return (
     <div className="Category">
