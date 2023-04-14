f70c5839180be9136bdb5571142a1511c6fc4241
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-14 15:21:31 +0600

    Add category filtering

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,18 +3,20 @@ import { AppContext } from "../../App";
 import "./ProductList.css";
 import { NavLink } from "react-router-dom";
 
-export default function ProductList() {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.map(product => (
-    <div>
-      <img src={product.picture} alt={product.name} />
-      <NavLink to={'/product/' + product.slug}>
-        {product.name}
-      </NavLink>
-      <span>{product.price} som</span>
-    </div>
-  ));
+  const output = products
+    .filter(product => product.category === category.id)
+    .map(product => (
+      <div key={product.id} className="Product">
+        <img src={product.picture} alt={product.name} />
+        <NavLink to={'/products/' + product.slug}>
+          {product.name}
+        </NavLink>
+        <span>{product.price} som</span>
+      </div>
+    ));
 
   return (
     <div className="ProductList">
