0f790645548ad1799d56b85e9c63c3cd911aab16
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-24 15:17:31 +0600

    Add orders page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore/lite";
-import { categoryCollection, onAuthChange, productsCollection } from "./firebase";
+import {
+  categoryCollection,
+  onAuthChange,
+  ordersCollection,
+  productsCollection,
+} from "./firebase";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import ThankYou from "./pages/ThankYou";
+import Orders from "./pages/Orders";
 
 // Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
   products: [],
+  orders: [],
+
   // контекст для корзины
   cart: {}, // содержимое корзинки
   setCart: () => {}, // изменить содержимое корзики
@@ -24,46 +32,69 @@ export const AppContext = createContext({
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
 
   const [cart, setCart] = useState(() => {
-    return JSON.parse(localStorage.getItem('cart')) || {};
+    return JSON.parse(localStorage.getItem("cart")) || {};
   });
 
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  useEffect(() => { // выполнить только однажды
+  useEffect(() => {
+    // выполнить только однажды
     getDocs(categoryCollection) // получить категории
-      .then(({ docs }) => { // когда категории загрузились
-        setCategories( // обновить состояние
-          docs.map(doc => ({ // новый массив
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setCategories(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
             ...doc.data(), // из свойств name, slug
-            id: doc.id // и свойства id
+            id: doc.id, // и свойства id
           }))
-        )
+        );
       });
 
     getDocs(productsCollection) // получить категории
-      .then(({ docs }) => { // когда категории загрузились
-        setProducts( // обновить состояние
-          docs.map(doc => ({ // новый массив
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setProducts(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
             ...doc.data(), // из свойств name, slug
-            id: doc.id // и свойства id
+            id: doc.id, // и свойства id
           }))
-        )
+        );
       });
 
-    onAuthChange(user => {
+    getDocs(ordersCollection) // получить категории
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setOrders(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
+            ...doc.data(), // из свойств name, slug
+            id: doc.id, // и свойства id
+          }))
+        );
+      });
+
+    onAuthChange((user) => {
       setUser(user);
     });
   }, []);
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products, cart, setCart, user }}>
+      <AppContext.Provider
+        value={{ categories, products, cart, setCart, user, orders }}
+      >
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -74,6 +105,7 @@ function App() {
             <Route path="/categories/:slug" element={<Category />} />
             <Route path="/products/:slug" element={<Product />} />
             <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="/orders" element={<Orders />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
