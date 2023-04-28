f0c69cb65d2ca340994edbf12f2aa91cdde6b3a9
Elaman Imashov <bobbyitkg@gmail.com>
2023-04-28 14:40:16 +0600

    Add category to firestore

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,4 +1,6 @@
+import { addDoc } from "firebase/firestore/lite";
 import { useState } from "react";
+import { categoryCollection } from "../../firebase";
 
 const AddCategory = () => {
   const [category, setCategory] = useState("");
@@ -8,7 +10,20 @@ const AddCategory = () => {
   }
 
   function onAddCategory() {
+    const name = category.trim();
 
+    if (name.length < 5) {
+      alert(
+        "Please provide a category name with minimum length of 5 characters."
+      );
+    }
+
+    addDoc(categoryCollection, {
+      name: name,
+      slug: name.replace(" ", "-").toLocaleLowerCase(),
+    }).then(() => {
+      setCategory("");
+    });
   }
 
   return (
